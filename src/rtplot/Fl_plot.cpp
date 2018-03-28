aa0df9ce45fe335f54b5ac2f147079271b894af3
Benjamin Navarro <navarro@lirmm.fr>
2018-03-28 15:57:56 +0200

    fix cursor coordinate display bug after resize

--- src/rtplot/Fl_plot.cpp
+++ src/rtplot/Fl_plot.cpp
@@ -45,7 +45,6 @@ Fl_Plot::Fl_Plot(int x, int y, int w, int h, const char *label) : Fl_Widget(x,y,
 	label_area_width_ = 200;
 	display_labels_ = false;
 	toggle_labels_ = false;
-	// displayLabels();
 
 	subdivisions_ = 6;
 
@@ -59,21 +58,179 @@ Fl_Plot::Fl_Plot(int x, int y, int w, int h, const char *label) : Fl_Widget(x,y,
 	display_labels_btn_->callback(&Fl_Plot::display_labels_btn_clb, this);
 }
 
+
+void Fl_Plot::refresh() {
+	redraw();
+	Fl::check();
+}
+
+void Fl_Plot::addPoint(int curve, float x, float y) {
+	curves_[curve].push_back(make_pair(x,y));
+	ordered_lists_[curve].first.insert(x);
+	ordered_lists_[curve].second.insert(y);
+
+	if(auto_xrange_) {
+		float min_val = std::numeric_limits<float>::infinity();
+		float max_val = -std::numeric_limits<float>::infinity();
+		for(auto& c_list : ordered_lists_) {
+			auto& list = c_list.second.first; // xvalues
+			float c_min = *list.begin();
+			float c_max = *(--list.end());
+			min_val = min(min_val, c_min);
+			max_val = max(max_val, c_max);
+		}
+		xrange_auto_.first = min_val;
+		xrange_auto_.second = max_val;
+	}
+	if(auto_yrange_) {
+		float min_val = std::numeric_limits<float>::infinity();
+		float max_val = -std::numeric_limits<float>::infinity();
+		for(auto& c_list : ordered_lists_) {
+			auto& list = c_list.second.second; // yvalues
+			float c_min = *list.begin();
+			float c_max = *(--list.end());
+			min_val = min(min_val, c_min);
+			max_val = max(max_val, c_max);
+		}
+		yrange_auto_.first = min_val;
+		yrange_auto_.second = max_val;
+	}
+
+}
+
+void Fl_Plot::removeFirstPoint(int curve) {
+	try {
+		auto& c = curves_.at(curve); // check for existance
+		if(c.begin() == c.end()) // check if non-empty
+			return;
+	}
+	catch (const std::out_of_range& oor) {
+		std::cerr << "Curve " << curve << " doesn't exist, can't remove a point from it\n";
+		return;
+	}
+
+	PointXY removed_point = curves_[curve].front();
+
+	curves_lock_.lock();
+	curves_[curve].pop_front();
+	curves_lock_.unlock();
+
+	auto& ordered_list = ordered_lists_[curve];
+
+	auto& xlist = ordered_list.first;
+	auto it = xlist.find(removed_point.first);
+	if(it != xlist.end()) { // Shouldn't be necessary
+		xlist.erase(it);
+	}
+
+	auto& ylist = ordered_list.second;
+	it = ylist.find(removed_point.second);
+	if(it != ylist.end()) { // Shouldn't be necessary
+		ylist.erase(it);
+	}
+
+}
+
+void Fl_Plot::displayLabels() {
+	if(not display_labels_) {
+		display_labels_ = true;
+		display_labels_btn_text_ = "-";
+
+		int max_text_width = 0;
+		for(auto& lbl : labels_) {
+			int texth=0, textw=0;
+			fl_measure(lbl.second.c_str(), textw, texth);
+			max_text_width = max(max_text_width, textw);
+		}
+		label_area_width_ = max_text_width;
+		if(label_area_width_)
+			label_area_width_ += 40;
+
+		auto w = static_cast<Fl_Window*>(parent());
+		int new_width = plot_offset_.first+plot_size_.first+plot_margins_.first+label_area_width_;
+		int new_height = plot_offset_.second+plot_size_.second+plot_margins_.second;
+		w->size_range(
+			new_width > Fl::w() ? Fl::w() : new_width,
+			new_height > Fl::h() ? Fl::h() : new_height,
+			0, 0, 0, 0, 1);
+
+
+		refresh();
+	}
+}
+void Fl_Plot::hideLabels() {
+	if(display_labels_) {
+		display_labels_ = false;
+		display_labels_btn_text_ = "+";
+		auto w = static_cast<Fl_Window*>(parent());
+		w->size_range(
+			plot_offset_.first+plot_size_.first+plot_margins_.first,
+			plot_offset_.second+plot_size_.second+plot_margins_.second,
+			0, 0, 0, 0, 1);
+
+
+		refresh();
+	}
+}
+
+void Fl_Plot::toggleLabels() {
+	toggle_labels_ = true;
+}
+
+void Fl_Plot::setSubdivisions(int sub) {
+	assert(sub > 0);
+	subdivisions_ = sub;
+}
+
+void Fl_Plot::setXRange(float min, float max) {
+	xrange_ = make_pair(min, max);
+	auto_xrange_ = false;
+}
+
+void Fl_Plot::setYRange(float min, float max) {
+	yrange_ = make_pair(min, max);
+	auto_yrange_ = false;
+}
+
+void Fl_Plot::setXLabel(const std::string& label) {
+	xlabel_ = label;
+}
+
+void Fl_Plot::setYLabel(const std::string& label) {
+	ylabel_ = label;
+}
+
+void Fl_Plot::setCurveLabel(int curve, const std::string& label) {
+	labels_[curve] = label;
+}
+
+void Fl_Plot::setAutoXRange() {
+	auto_xrange_ = true;
+}
+
+void Fl_Plot::setAutoYRange() {
+	auto_yrange_ = true;
+}
+
+void Fl_Plot::display_labels_btn_clb(Fl_Widget *w, void *obj) {
+	static_cast<Fl_Plot*>(obj)->toggleLabels();
+}
+
 void Fl_Plot::draw() {
 	Fl_Color init_color = fl_color();
 
 	int label_width = display_labels_ ? label_area_width_ : 0;
 	auto w = static_cast<Fl_Window*>(parent());
 	plot_size_ = make_pair(
-	    w->w()-plot_offset_.first-plot_margins_.first-label_width,
-	    w->h()-plot_offset_.second-plot_margins_.second);
+		w->w()-plot_offset_.first-plot_margins_.first-label_width,
+		w->h()-plot_offset_.second-plot_margins_.second);
+	this->resize(plot_offset_.first, plot_offset_.second, plot_size_.first, plot_size_.second);
 	display_labels_btn_->resize(plot_offset_.first+plot_size_.first+5, plot_offset_.second, 20, 20);
 
 	drawBackground();
 	drawAxes();
 	if(toggle_labels_) {
 		toggle_labels_ = false;
-		// display_labels_ = not display_labels_;
 		if(display_labels_) {
 			hideLabels();
 		}
@@ -111,11 +268,35 @@ void Fl_Plot::draw() {
 	fl_pop_clip();
 
 	if(display_cursor_coordinates_) {
+		PointXY p = scaleToGraph(last_cursor_position_);
 		fl_color(FL_BLACK);
-		fl_draw(cursor_coordinates_.c_str(), 10, parent()->h()-10);
+		fl_draw((to_string(p.first) + ", " + to_string(p.second)).c_str(), 10, parent()->h()-10);
 	}
 
 	fl_color(init_color);
+}
+
+int Fl_Plot::handle(int event) {
+	int ret_code = 0;
+	switch(event) {
+	case FL_ENTER:
+		display_cursor_coordinates_ = true;
+		ret_code = 1;
+		break;
+	case FL_LEAVE:
+		display_cursor_coordinates_ = false;
+		ret_code = 1;
+		break;
+	case FL_MOVE:
+	{
+		last_cursor_position_ = std::make_pair(Fl::event_x(), Fl::event_y());
+		ret_code = 1;
+	}
+	break;
+	default:
+		break;
+	}
+	return ret_code;
 }
 
 void Fl_Plot::drawAxes() {
@@ -220,170 +401,6 @@ void Fl_Plot::drawLabels() {
 	fl_color(orig_col);
 }
 
-int Fl_Plot::handle(int event) {
-	int ret_code = 0;
-	switch(event) {
-	case FL_ENTER:
-		display_cursor_coordinates_ = true;
-		ret_code = 1;
-		break;
-	case FL_LEAVE:
-		display_cursor_coordinates_ = false;
-		ret_code = 1;
-		break;
-	case FL_MOVE:
-	{
-		PointXY p = scaleToGraph(make_pair(Fl::event_x(), Fl::event_y()));
-		cursor_coordinates_ = to_string(p.first) + ", " + to_string(p.second);
-		ret_code = 1;
-	}
-	break;
-	default:
-		break;
-	}
-	return ret_code;
-}
-
-void Fl_Plot::addPoint(int curve, float x, float y) {
-	curves_[curve].push_back(make_pair(x,y));
-	ordered_lists_[curve].first.insert(x);
-	ordered_lists_[curve].second.insert(y);
-
-	if(auto_xrange_) {
-		float min_val = std::numeric_limits<float>::infinity();
-		float max_val = -std::numeric_limits<float>::infinity();
-		for(auto& c_list : ordered_lists_) {
-			auto& list = c_list.second.first; // xvalues
-			float c_min = *list.begin();
-			float c_max = *(--list.end());
-			min_val = min(min_val, c_min);
-			max_val = max(max_val, c_max);
-		}
-		xrange_auto_.first = min_val;
-		xrange_auto_.second = max_val;
-	}
-	if(auto_yrange_) {
-		float min_val = std::numeric_limits<float>::infinity();
-		float max_val = -std::numeric_limits<float>::infinity();
-		for(auto& c_list : ordered_lists_) {
-			auto& list = c_list.second.second; // yvalues
-			float c_min = *list.begin();
-			float c_max = *(--list.end());
-			min_val = min(min_val, c_min);
-			max_val = max(max_val, c_max);
-		}
-		yrange_auto_.first = min_val;
-		yrange_auto_.second = max_val;
-	}
-	return;
-}
-
-void Fl_Plot::removeFirstPoint(int curve) {
-	try {
-		auto& c = curves_.at(curve); // check for existance
-		if(c.begin() == c.end()) // check if non-empty
-			return;
-	}
-	catch (const std::out_of_range& oor) {
-		std::cerr << "Curve " << curve << " doesn't exist, can't remove a point from it\n";
-		return;
-	}
-
-	PointXY removed_point = curves_[curve].front();
-
-	curves_lock_.lock();
-	curves_[curve].pop_front();
-	curves_lock_.unlock();
-
-	auto& ordered_list = ordered_lists_[curve];
-
-	auto& xlist = ordered_list.first;
-	auto it = xlist.find(removed_point.first);
-	if(it != xlist.end()) { // Shouldn't be necessary
-		xlist.erase(it);
-	}
-
-	auto& ylist = ordered_list.second;
-	it = ylist.find(removed_point.second);
-	if(it != ylist.end()) { // Shouldn't be necessary
-		ylist.erase(it);
-	}
-}
-
-void Fl_Plot::displayLabels() {
-	if(not display_labels_) {
-		display_labels_ = true;
-		display_labels_btn_text_ = "-";
-
-		int max_text_width = 0;
-		for(auto& lbl : labels_) {
-			int texth=0, textw=0;
-			fl_measure(lbl.second.c_str(), textw, texth);
-			max_text_width = max(max_text_width, textw);
-		}
-		label_area_width_ = max_text_width;
-		if(label_area_width_)
-			label_area_width_ += 40;
-
-		auto w = static_cast<Fl_Window*>(parent());
-		// w->size_range(window_init_size_.first+label_area_width_, window_init_size_.second, 0, 0, 0, 0, 1);
-		int new_width = plot_offset_.first+plot_size_.first+plot_margins_.first+label_area_width_;
-		int new_height = plot_offset_.second+plot_size_.second+plot_margins_.second;
-		w->size_range(
-		    new_width > Fl::w() ? Fl::w() : new_width,
-		    new_height > Fl::h() ? Fl::h() : new_height,
-		    0, 0, 0, 0, 1);
-		// parent()->resize(parent()->x(),parent()->y(),parent()->w()+label_area_width_, parent()->h());
-		refresh();
-	}
-}
-void Fl_Plot::hideLabels() {
-	if(display_labels_) {
-		display_labels_ = false;
-		display_labels_btn_text_ = "+";
-		auto w = static_cast<Fl_Window*>(parent());
-		// w->size_range(window_init_size_.first, window_init_size_.second, 0, 0, 0, 0, 1);
-		w->size_range(
-		    plot_offset_.first+plot_size_.first+plot_margins_.first,
-		    plot_offset_.second+plot_size_.second+plot_margins_.second,
-		    0, 0, 0, 0, 1);     // parent()->resize(parent()->x(),parent()->y(),parent()->w()-label_area_width_, parent()->h());
-		refresh();
-	}
-}
-
-void Fl_Plot::toggleLabels() {
-	toggle_labels_ = true;
-}
-
-
-
-void Fl_Plot::refresh() {
-	redraw();
-	Fl::check();
-}
-
-void Fl_Plot::setXRange(float min, float max) {
-	xrange_ = make_pair(min, max);
-	auto_xrange_ = false;
-}
-
-void Fl_Plot::setYRange(float min, float max) {
-	yrange_ = make_pair(min, max);
-	auto_yrange_ = false;
-}
-
-void Fl_Plot::setXLabel(const std::string& label) {
-	xlabel_ = label;
-}
-
-void Fl_Plot::setYLabel(const std::string& label) {
-	ylabel_ = label;
-}
-
-void Fl_Plot::setCurveLabel(int curve, const std::string& label) {
-	labels_[curve] = label;
-}
-
 void Fl_Plot::initScaleToPlot() {
 	current_xrange_ = auto_xrange_ ? xrange_auto_ : xrange_;
 	current_yrange_ = auto_yrange_ ? yrange_auto_ : yrange_;
@@ -412,10 +429,6 @@ void Fl_Plot::printPoint(const PointXY& point) {
 	cout << "(" << point.first << "," << point.second << ")";
 }
 
-void Fl_Plot::setSubdivisions(int sub) {
-	assert(sub > 0);
-	subdivisions_ = sub;
-}
 
 void Fl_Plot::drawXTickValue(float num, const PointXY& point) {
 
@@ -433,16 +446,4 @@ void Fl_Plot::drawYTickValue(float num, const PointXY& point) {
 	int textw = 0, texth = 0;
 	fl_measure(str, textw, texth);
 	fl_draw(str, point.first-textw-5, point.second+texth/2-2);
-}
-
-void Fl_Plot::setAutoXRange() {
-	auto_xrange_ = true;
-}
-
-void Fl_Plot::setAutoYRange() {
-	auto_yrange_ = true;
-}
-
-void Fl_Plot::display_labels_btn_clb(Fl_Widget *w, void *obj) {
-	static_cast<Fl_Plot*>(obj)->toggleLabels();
 }
